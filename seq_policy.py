1f2b93c45e78970b8c1f4e08da4cc39c89e860f0
Dr-Quantum <56049888+Dr-Quantum@users.noreply.github.com>
2024-04-30 16:29:25 -0400

    Update seq_policy.py

--- seq_policy.py
+++ seq_policy.py
@@ -1,10 +1,21 @@
 import json
 import numpy as np
+import sys, os, time
+from math import inf
 
-def policy_reader(file_name):
-    jsonFile =  open(file_name, encoding="utf-8-sig") 
-    policy_data = json.load(jsonFile)
-    return policy_data
+from sequence.topology.node import Node
+from sequence.components.memory import MemoryWithRandomCoherenceTime
+from sequence.entanglement_management.generation import EntanglementGenerationA, GenProtoStatus
+from sequence.kernel.timeline import Timeline
+from sequence.kernel.process import Process
+from sequence.kernel.event import Event
+from sequence.topology.node import BSMNode
+from sequence.components.optical_channel import QuantumChannel, ClassicalChannel
+from sequence.entanglement_management.entanglement_protocol import EntanglementProtocol
+from sequence.entanglement_management.swapping import EntanglementSwappingA, EntanglementSwappingB
+from sequence.resource_management.resource_manager import ResourceManagerMessage, ResourceManagerMsgType
+from sequence.message import Message
+
 
 class policy():
     def __init__(self, policy_data) -> None:
@@ -16,12 +27,14 @@ class policy():
             self.states.append(np.array(policy_data['state_info'][i]['state']))
             self.action_spaces.append(policy_data['state_info'][i]['action_space'])
             self.policies.append(policy_data['state_info'][i]['policy'])
+   
+    def policy_reader(file_name):
+        jsonFile =  open(file_name, encoding="utf-8-sig") 
+        policy_data = json.load(jsonFile)
+        return policy_data
 
 
 
-policy_data_ = policy_reader('policy_n3_test.json')
-
-policy_1 = policy(policy_data_)
 
 
 
